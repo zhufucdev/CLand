d7d82dfc09998c23d558bdad93a1dcc2cb927cdc
zhufucdev <1095291434@qq.com>
2022-08-30 07:20:12 +0800

    Initial Commit

---
+++ CLand/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CLand
+//
+//  Created by 朱拂 on 2022/8/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
