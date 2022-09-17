05ef33c3af46e5b7bd907e3183cb850f54410040
zhufucdev <1095291434@qq.com>
2022-09-18 07:04:05 +0800

    Implementation

--- CLand/main.c
+++ CLand/main.c
@@ -8,7 +8,19 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n, s = 0;
+    scanf("%d", &n);
+    int d[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &d[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (d[i] > d[j]) {
+                s ++;
+            }
+        }
+    }
+    printf("%d\n", s);
     return 0;
 }
