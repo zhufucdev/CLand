29746b45aad84d92823ff494f92f22fcbddede67
zhufucdev <1095291434@qq.com>
2022-11-03 16:33:31 +0800

    Added arrangement

--- README.md
+++ README.md
@@ -1,29 +1,40 @@
-# CLand
+# Arrange
 
-Collection of my example code produced while learning
-the C programming language.
+Input: **int** _n_
 
-# How
+Output: all possible arrangements (counting to _n!_) of array 123...n
 
-These code are presented as small fragments, which
-correspond to different branches.
+# Example
 
-Hope to see them all nicely documented.
-
-# License
-
+Input:
 ```
-Copyright 2022 zhufucdev
+4
+```
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Output:
+```
+1 2 3 4 
+1 2 4 3 
+1 3 2 4 
+1 3 4 2 
+1 4 2 3 
+1 4 3 2 
+2 1 3 4 
+2 1 4 3 
+2 3 1 4 
+2 3 4 1 
+2 4 1 3 
+2 4 3 1 
+3 1 2 4 
+3 1 4 2 
+3 2 1 4 
+3 2 4 1 
+3 4 1 2 
+3 4 2 1 
+4 1 2 3 
+4 1 3 2 
+4 2 1 3 
+4 2 3 1 
+4 3 1 2 
+4 3 2 1 
 ```
--- main.c
+++ main.c
@@ -3,9 +3,113 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#define CACHE_SIZE 10
+
+int *f_cache;
+
+int factorial(int x) {
+    if (f_cache[x]) {
+        return f_cache[x];
+    }
+
+    int s = 1;
+    for (int i = 2; i <= x; i++) {
+        s *= i;
+    }
+
+    f_cache[x] = s;
+    return s;
+}
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+void fix(int i, int n, char **arr, char *changes) {
+    for (int j = 0; j < n; j++) {
+        int period = factorial(n - j - 1);
+        if (i % period == 0) {
+            char choice = 0;
+            for (int k = 0; k < n; k++) {
+                int contains = 0;
+                for (int l = 0; l < j; l++) {
+                    if (k == arr[i][l]) {
+                        contains = 1;
+                        break;
+                    }
+                }
+                if (!contains) {
+                    int previous = factorial(n - j);
+                    for (int l = max(0, i / previous * previous); l < i; l += period) {
+                        if (k == arr[l][j]) {
+                            contains = 1;
+                            break;
+                        }
+                    }
+                }
+                if (!contains) {
+                    choice = k;
+                    break;
+                }
+            }
+            arr[i][j] = choice;
+            changes[j] ++;
+        } else {
+            arr[i][j] = arr[i - 1][j];
+        }
+    }
+}
+
+char **arrange(int n) {
+    int rows = factorial(n);
+    char **result = (char**) calloc(rows, sizeof(*result));
+    char *changes = (char*) calloc(rows, sizeof(char));
+    for (int i = 0; i < rows; i++) {
+        result[i] = (char*) calloc(n, sizeof(char) * n);
+        fix(i, n, result, changes);
+    }
+    free(changes);
+
+    return result;
+}
+
+void test(int n, char **arr) {
+    int failed = 0;
+    for (int y = 0; y < factorial(n); y ++) {
+        for (int x = 0; x < y; x++) {
+            int equals = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[y][i] == arr[x][i]) {
+                    equals ++;
+                }
+            }
+            if (equals == n) {
+                printf("Test %d failed.\n", y);
+                failed = 1;
+            }
+        }
+    }
+    if (!failed) {
+        printf("Test succeeded.\n");
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    f_cache = (int*) calloc(CACHE_SIZE, sizeof(int));
+
+    int n;
+    scanf("%d", &n);
+
+    char **arr = arrange(n);
+    for (int i = 0; i < factorial(n); i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", (int) arr[i][j] + 1);
+        }
+        printf("\n");
+    }
+//    test(n, arr);
+    free(arr);
     return 0;
 }
+
