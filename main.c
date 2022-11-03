620dc8529899b60d89a3b2d25bdcb4b65104ad76
zhufucdev <1095291434@qq.com>
2022-11-03 16:30:32 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+//
+// Created by 朱拂 on 2022/11/3.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
