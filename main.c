e66856ccd4b1b330fee1ac27f8dcb981ef108015
zhufucdev <1095291434@qq.com>
2022-11-03 16:43:30 +0800

    Added monkey peaches

--- main.c
+++ main.c
@@ -4,8 +4,29 @@
 
 #include <stdio.h>
 
+int iterate(int day, int amount);
+
+int loop(void);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int x = iterate(9, 1);
+    int y = loop();
+    if (x == y)
+        printf("%d\n", x);
+    else
+        printf("inconsistent results: %d and %d", x, y);
     return 0;
 }
+
+int loop(void) {
+    int amount = 1;
+    for (int i = 0; i < 9; i++) {
+        amount = (amount + 1) * 2;
+    }
+    return amount;
+}
+
+int iterate(int day, int amount) {
+    return day == 0 ? amount : iterate(day - 1, (amount + 1) * 2);
+}
+
